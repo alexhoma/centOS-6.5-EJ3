0cbce514f60a6921dd9c1782a7e7a9a3f5fa1de6
Alex Casajuana <alexcm.14@gmail.com>
2015-11-15 20:28:30 +0100

    Cambio path del clone del repo de mi modulo customizado

--- vagrant/scripts/install_modules.sh
+++ vagrant/scripts/install_modules.sh
@@ -14,7 +14,8 @@ puppet module install example42/php --modulepath $dir --version 2.0.25
 puppet module install puppetlabs/mysql --modulepath $dir --version 3.6.1
 
 # MY_MODULE
-git clone https://github.com/alexhoma/alexhoma-custom_module.git $dir
+git clone https://github.com/alexhoma/alexhoma-custom_module.git
+mv alexhoma-custom_module/ $dir
 
 # MEMCACHED
 puppet module install saz/memcached --modulepath $dir --version 2.8.1

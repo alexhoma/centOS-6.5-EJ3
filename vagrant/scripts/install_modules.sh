8d3ae171824c5f56fe4919e5a6dee5eb876e4ee4
Alex Casajuana <alexcm.14@gmail.com>
2015-11-15 20:22:05 +0100

    Reparación parametros de los modulos

--- vagrant/scripts/install_modules.sh
+++ vagrant/scripts/install_modules.sh
@@ -10,11 +10,11 @@ export LC_ALL
 dir='/vagrant/modules/'
 puppet module install puppetlabs/apache --modulepath $dir --version 1.6.0
 puppet module install example42/yum --modulepath $dir --version 2.1.25
-puppet module install example42/php $dir -version 2.0.25
-puppet module install puppetlabs/mysql $dir -version 3.6.1
+puppet module install example42/php --modulepath $dir --version 2.0.25
+puppet module install puppetlabs/mysql --modulepath $dir --version 3.6.1
 
 # MY_MODULE
 git clone https://github.com/alexhoma/alexhoma-custom_module.git $dir
 
 # MEMCACHED
-puppet module install saz/memcached $dir -version 2.8.1
+puppet module install saz/memcached --modulepath $dir --version 2.8.1
